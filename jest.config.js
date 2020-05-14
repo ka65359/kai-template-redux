7a2fc7fc4b5544a22710fded30c437ea4fb1cedf
Kai <tehhavok@gmail.com>
2020-05-13 23:43:25 -0400

    add routes to store for jest tests

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,11 @@ module.exports = exports = {
   moduleFileExtensions: ["js", "json", "jsx", "node"],
   testPathIgnorePatterns: ["/node_modules/"],
   moduleNameMapper: {
+    "^store$": require.resolve("./src/store"),
+    "^store/actions$": require.resolve("./src/store/actions"),
+    "^store/actions(.+)": require.resolve("./src/store/actions/$1"),
+    "^store/selectors": require.resolve("./src/store/selectors"),
+    "^store/selectors(.+)": require.resolve("./src/store/selectors/$1"),
     "\\.(css|scss)$": require.resolve("./test/style-mock"),
     "\\.(gif|jpg|png|svg)$": require.resolve("./test/file-mock.js")
   },
