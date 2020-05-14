b3f3ab88561bfe70053664cb03d4f1d8abe0d141
Kai <tehhavok@gmail.com>
2020-05-14 15:07:45 -0400

    Add actions and reducers

--- src/store/reducers/ui-reducers/index.js
+++ src/store/reducers/ui-reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import sample from "./sample-reducer";
+import tictactoe from "./tictactoe-reducer";
 
 export default combineReducers({
-  sample
+  tictactoe
 });
